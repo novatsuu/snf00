9fa60396abf67b9d04af8a8111bdeced3ee6b02e
Hüseyin ÖZTÜRK <wys-workshop@outlook.fr>
2025-02-10 19:16:05 +0100

    creating Godot Env.

---
+++ godot/packer/Packer.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+// Don't call this "SNF Zero Zero", but "SNCF Zero * 2"
+
+namespace Snf00.Packer;
+
+public partial class Packer : Node
+{
+	private string Path;
+
+	public void Update() 
+	{
+		
+	}
+}

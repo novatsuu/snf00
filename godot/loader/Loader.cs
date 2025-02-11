a7dbf02bbd7f5782e2391e1648535ad218d529e4
Hüseyin ÖZTÜRK <wys-workshop@outlook.fr>
2025-02-11 16:20:30 +0100

    coding the Snf Shell

--- godot/loader/Loader.cs
+++ godot/loader/Loader.cs
@@ -1,14 +1,35 @@
 using Godot;
-using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Snf00.Loader;
 
 public partial class Loader : Node
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public string[] GetProgramsInDir(string dir)
 	{
+		var rslt = Directory.GetFiles(dir);
+		List<string> selected = [];
 
+		foreach (var item in rslt)
+		{
+			if (item.EndsWith(".gd") || item.EndsWith(".snfsh"))
+			{
+				_ = selected.Append(item);
+			}
+		}
+
+		return [.. selected];
 	}
 
+	public void LoadProgramFile(GDScript script, string program)
+	{
+		StreamReader reader = new(program);
+		while (!reader.EndOfStream) 
+		{
+			script.SourceCode += reader.ReadLine();
+		}
+		reader.Close();
+	}
 }
--- godot/snfshell/snfshell.gd
+++ godot/snfshell/snfshell.gd
@@ -4,13 +4,38 @@ extends Node
 @onready var r_output: RichTextLabel = $rOutput
 
 var input_buffer : String = ''
+var path : String = './'
+var loader := preload("res://loader/Loader.cs").new()
+var app_base := preload("res://loader/app_base.tscn")
+var runtime : Dictionary = {}
+
+func gename(length: int) -> String:
+	var rng = RandomNumberGenerator.new()
+	var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	var random_string = ""
+	for i in range(length):
+		random_string += charset[rng.randi_range(0, charset.length() - 1)]
+	return random_string
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 func execute(cmd: Array) -> int:
-	# TODO call & return the main function.
+	var loaded : Array = loader.GetProgramsInDir(path)
+	
+	if not loaded.find(loaded) == -1:
+		r_output.text += '\n[color=red]snfsh: not found:\t' + cmd[0] + '[/color]'
+	else:
+		var node = app_base.instantiate()
+		node.name = cmd[0]+'@'+gename(8)
+		runtime[node.name] = node
+		add_child(node)
+		# Load the program
+		loader.LoadProgramFile(node.script, cmd[0])
+		node.script.reload(true)
+		node.main(cmd.slice(1))
 	return 0;
 
 func _on_text_edit_text_changed() -> void:
@@ -25,7 +50,8 @@ func _input(event: InputEvent) -> void:
 	if Input.is_action_pressed("ui_text_newline"):
 		if not input_buffer.strip_edges().is_empty():
 			var argv = input_buffer.strip_edges().split(' ', false)
-			execute(argv.slice(1))
-		else: r_output.text += '/snf/> '
+			execute(argv)
+		r_output.text += '\n@snf:' + path
+		
 		# Clear the buffer.
 		input_buffer = ''
